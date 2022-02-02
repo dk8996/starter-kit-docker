b5b2eb01b6b5f98a9983d1b6eaab8103a4621535
Dimitry Kudryavtsev <dk8996@gmail.com>
2022-02-02 09:07:12 -0600

    Upgrading the solidity version.

--- truffle-suite/src/contracts/EthSwap.sol
+++ truffle-suite/src/contracts/EthSwap.sol
@@ -1,6 +1,5 @@
-//SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.6.0 <= 0.8.11;
 
 contract EthSwap {
     string public name = "EthSwap Instant Exchange";
