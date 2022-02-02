d69909f1dd0c57e5321b082d4d19ec9a540fe847
Dimitry Kudryavtsev <dk8996@gmail.com>
2022-02-02 09:02:23 -0600

    Fixing solidity version

--- truffle-suite/src/contracts/Migrations.sol
+++ truffle-suite/src/contracts/Migrations.sol
@@ -1,25 +1,19 @@
-//SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.6.0 <= 0.8.11;
 
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
 
-  constructor() public {
-    owner = msg.sender;
-  }
-
   modifier restricted() {
     if (msg.sender == owner) _;
   }
 
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
+  constructor() {
+    owner = msg.sender;
   }
 
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
   }
 }
