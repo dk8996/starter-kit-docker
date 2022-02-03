c695e429b5247511d974144fd2db1fccc671904a
Dimitry Kudryavtsev <dk8996@gmail.com>
2022-02-02 19:29:04 -0600

    Add test

--- truffle-suite/src/contracts/EthSwap.sol
+++ truffle-suite/src/contracts/EthSwap.sol
@@ -8,12 +8,28 @@ contract EthSwap {
     Token public token;
     uint rate = 100;
 
+    event TokenPurchased(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    );
+
     constructor(Token _token) {
         token = _token;
     }
 
     function buyTokens() public payable {
+        // Calculate the number of tokens to buy
         uint256 tokenAmount = msg.value * rate;
+
+        // Require that EthSwap has enough tokens
+        require(token.balanceOf(address(this)) >= tokenAmount);
+
+        // Transfer tokens to the user
         token.transfer(msg.sender, tokenAmount);
+
+        // Emit an event
+        emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
     }
 }
